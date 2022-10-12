ecfde88458195886167b8ad29e0c0af6ddd0bfc9
Juan Trebolle <jtr@jysk.com>
2022-10-12 10:58:30 +0200

    Removed ugly tabbar

--- LiSIGN/View/CustomTabBar/AppTabBarView.swift
+++ LiSIGN/View/CustomTabBar/AppTabBarView.swift
@@ -10,7 +10,6 @@ struct AppTabBarView: View {
     @State private var tabSelection: TabBarItem = .Rooms
     
     var body: some View {
-        defaultTabBarView;
         CustomTabBarContainerView(selection: $tabSelection) {
             Color.blue
                 .tabBarItem(tab: .Rooms, selection: $tabSelection)
@@ -32,33 +31,5 @@ struct AppTabBar_Previews: PreviewProvider {
     
     static var previews: some View {
         AppTabBarView()
-    }
-}
-
-extension AppTabBarView {
-    private var defaultTabBarView: some View {
-        TabView(selection: $selection) {
-            Color.red
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Rooms")
-                }
-            Color.blue
-                .tabItem {
-                    Image(systemName: "eurosign")
-                    Text("Marketplace")
-                }
-            Color.orange
-                .tabItem {
-                    Image(systemName: "lamp.desk")
-                    Text("Objects")
-                }
-            Color.cyan
-                .tabItem {
-                    Image(systemName: "heart")
-                    Text("Favorites")
-                }
-        }
-
     }
 }
