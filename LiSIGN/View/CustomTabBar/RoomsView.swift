09db35f893b519502f9d6197ec2dfb3968da02c7
Juan Trebolle <jtr@jysk.com>
2022-10-15 18:58:23 +0200

    added ProductCard

--- LiSIGN/View/CustomTabBar/RoomsView.swift
+++ LiSIGN/View/CustomTabBar/RoomsView.swift
@@ -25,7 +25,9 @@ struct RoomsView: View {
                     ForEach(myFruits.filter({ (fruit: String) -> Bool in
                         return fruit.hasPrefix(searchText) || searchText == ""
                     }), id: \.self) { fruit in
-                        Text(fruit)
+//                        // Text(fruit)
+                        ProductCard(image: "./bad3", title: fruit, type: "room", price: 11.99)
+
                     }
                 }
                 .listStyle(GroupedListStyle())
@@ -88,6 +90,51 @@ struct SearchBar: View {
         .frame(height: 40)
         .cornerRadius(13)
         .padding()
+    }
+}
+
+struct CardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 0)
+    }
+}
+
+struct ProductCard: View {
+    var image: String
+    var title: String
+    var type: String
+    var price: Double
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .padding(.all, 20)
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.system(size: 26, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                Text(type)
+                    .font(.system(size: 16, weight: .bold, design: .default))
+                    .foregroundColor(.gray)
+                HStack {
+                    Text("$" + String.init(format: "%0.2f", price))
+                        .font(.system(size: 16, weight: .bold, design: .default))
+                        .foregroundColor(.white)
+                        .padding(.top, 8)
+                }
+            }.padding(.trailing, 20)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .background(Color(red: 32/255, green: 36/255, blue: 38/255))
+        .modifier(CardModifier())
+        .padding(.all, 10)
     }
 }
 
