ad434d7e76bbcee28745adb524ae6420513a423f
Juan Trebolle <jtr@jysk.com>
2022-10-15 19:38:53 +0200

    made ProductCard reusable

--- LiSIGN/View/CustomTabBar/RoomsView.swift
+++ LiSIGN/View/CustomTabBar/RoomsView.swift
@@ -54,12 +54,6 @@ struct RoomsView: View {
     }
 }
 
-struct RoomsView_Previews: PreviewProvider {
-    static var previews: some View {
-        RoomsView()
-    }
-}
-
 struct SearchBar: View {
     @Binding var searchText: String
     @Binding var searching: Bool
@@ -93,50 +87,12 @@ struct SearchBar: View {
     }
 }
 
-struct CardModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .cornerRadius(20)
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 0)
+struct RoomsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomsView()
     }
 }
 
-struct ProductCard: View {
-    var image: String
-    var title: String
-    var type: String
-    var price: Double
-    
-    var body: some View {
-        HStack(alignment: .center) {
-            Image(image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100)
-                .padding(.all, 20)
-            
-            VStack(alignment: .leading) {
-                Text(title)
-                    .font(.system(size: 26, weight: .bold, design: .default))
-                    .foregroundColor(.white)
-                Text(type)
-                    .font(.system(size: 16, weight: .bold, design: .default))
-                    .foregroundColor(.gray)
-                HStack {
-                    Text("$" + String.init(format: "%0.2f", price))
-                        .font(.system(size: 16, weight: .bold, design: .default))
-                        .foregroundColor(.white)
-                        .padding(.top, 8)
-                }
-            }.padding(.trailing, 20)
-            Spacer()
-        }
-        .frame(maxWidth: .infinity, alignment: .center)
-        .background(Color(red: 32/255, green: 36/255, blue: 38/255))
-        .modifier(CardModifier())
-        .padding(.all, 10)
-    }
-}
 
 // This next extension should be in the file ContentView.swift ?? not sure about if it's necessary
 extension UIApplication {
---
+++ LiSIGN/View/shared/ProductCard.swift
@@ -0,0 +1,57 @@
+//
+//  ProductCard.swift
+//  LiSIGN
+//
+
+import SwiftUI
+
+struct CardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 0)
+    }
+}
+
+struct ProductCard: View {
+    var image: String
+    var title: String
+    var type: String
+    var price: Double
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .padding(.all, 20)
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.system(size: 26, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                Text(type)
+                    .font(.system(size: 16, weight: .bold, design: .default))
+                    .foregroundColor(.gray)
+                HStack {
+                    Text("$" + String.init(format: "%0.2f", price))
+                        .font(.system(size: 16, weight: .bold, design: .default))
+                        .foregroundColor(.white)
+                        .padding(.top, 8)
+                }
+            }.padding(.trailing, 20)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .background(Color(red: 32/255, green: 36/255, blue: 38/255))
+        .modifier(CardModifier())
+        .padding(.all, 10)
+    }
+}
+
+struct ProductCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCard(image: "3d_scanned_room", title: "test", type: "room", price: 0.99)
+    }
+}
