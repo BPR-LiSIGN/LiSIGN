1c69e6b692010ef09d75494c0d94fa7bf9da16ee
Juan Trebolle <jtr@jysk.com>
2022-11-23 15:29:57 +0100

    Update room model

--- LiSIGN/RoomModel.swift
+++ LiSIGN/RoomModel.swift
@@ -7,49 +7,98 @@ import Foundation
 import SwiftUI
 import ModelIO
 
-class Room {
-    private var title: String
-    private var image: Image
-    private var type: String
-    private var roomObject: MDLAsset // MDLAsset is the type that LiDAR gives back for the 3D room scan
-
-    init(title: String, image: Image, type: String, roomObject: MDLAsset) {
-        self.title = title
-        self.image = image
-        self.type = type
-        self.roomObject = roomObject
-    }
-
-    func getTitle() -> String{
-        return self.title
-    }
-
-    func setTitle(title: String) {
-        self.title = title
-    }
-
-    func getImage() -> Image{
-        return self.image
-    }
-
-    func setImage(image: Image) {
-        self.image = image
-    }
-
-    func getType() -> String{
-        return self.type
-    }
-
-    func setType(type: String) {
-        self.type = type
+public class Room {
+//    private var title: String
+//    private var image: Image
+//    private var type: String
+//    private var roomObject: MDLAsset // MDLAsset is the type that LiDAR gives back for the 3D room scan
+//
+//    init(title: String, image: Image, type: String, roomObject: MDLAsset) {
+//        self.title = title
+//        self.image = image
+//        self.type = type
+//        self.roomObject = roomObject
+//    }
+//
+//    func getTitle() -> String{
+//        return self.title
+//    }
+//
+//    func setTitle(title: String) {
+//        self.title = title
+//    }
+//
+//    func getImage() -> Image{
+//        return self.image
+//    }
+//
+//    func setImage(image: Image) {
+//        self.image = image
+//    }
+//
+//    func getType() -> String{
+//        return self.type
+//    }
+//
+//    func setType(type: String) {
+//        self.type = type
+//    }
+//
+//    func getRoomObject() -> MDLAsset {
+//        return self.roomObject
+//    }
+//
+//    func setRoomObject(roomObject: MDLAsset) {
+//        self.roomObject = roomObject
+//    }
+    private var _id: Int = 0    // Maybe we don't need an id at all
+    private var _title: String = ""
+    private var _image: String = ""
+    private var _type: String = ""
+    private var _roomObject: MDLAsset // MDLAsset is the type that LiDAR gives back for the 3D room scan
+    
+    public init(roomObject: MDLAsset) {
+        self._roomObject = roomObject
+        // For now, the rest of the properties are hardcoded
+        self.title = "Scan 1"
+        self.type = "Room"
+        // TODO: missing id property
     }
     
-    func getRoomObject() -> MDLAsset {
-        return self.roomObject
+    public var title: String {
+        get {
+            return self._title;
+        }
+        set {
+            self._title = newValue
+        }
     }
     
-    func setRoomObject(roomObject: MDLAsset) {
-        self.roomObject = roomObject
+    public var image: String {
+        get {
+            return self._image;
+        }
+        set {
+            self._image = newValue
+        }
+    }
+    
+    public var type: String {
+        get {
+            return self._type;
+        }
+        set {
+            self._type = newValue
+        }
+    }
+    
+    public var roomObject: String {
+        get {
+            return self._roomObject;
+        }
+        set {
+            self._roomObject = newValue
+        }
     }
 }
 
