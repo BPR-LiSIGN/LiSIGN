c24f609baf5c785b6b173abd9aa2199acf7bc0c3
Juan Trebolle <jtr@jysk.com>
2022-11-24 22:15:20 +0100

    refactor Room model

--- LiSIGN/RoomModel.swift
+++ LiSIGN/RoomModel.swift
@@ -8,50 +8,7 @@ import SwiftUI
 import ModelIO
 
 public class Room {
-//    private var title: String
-//    private var image: Image
-//    private var type: String
-//    private var roomObject: MDLAsset // MDLAsset is the type that LiDAR gives back for the 3D room scan
-//
-//    init(title: String, image: Image, type: String, roomObject: MDLAsset) {
-//        self.title = title
-//        self.image = image
-//        self.type = type
-//        self.roomObject = roomObject
-//    }
-//
-//    func getTitle() -> String{
-//        return self.title
-//    }
-//
-//    func setTitle(title: String) {
-//        self.title = title
-//    }
-//
-//    func getImage() -> Image{
-//        return self.image
-//    }
-//
-//    func setImage(image: Image) {
-//        self.image = image
-//    }
-//
-//    func getType() -> String{
-//        return self.type
-//    }
-//
-//    func setType(type: String) {
-//        self.type = type
-//    }
-//
-//    func getRoomObject() -> MDLAsset {
-//        return self.roomObject
-//    }
-//
-//    func setRoomObject(roomObject: MDLAsset) {
-//        self.roomObject = roomObject
-//    }
-    private var _id: Int = 0    // Maybe we don't need an id at all
+    private var _id: UUID = UUID()    // Maybe we don't need an id at all
     private var _title: String = ""
     private var _image: String = ""
     private var _type: String = ""
@@ -60,9 +17,8 @@ public class Room {
     public init(roomObject: MDLAsset) {
         self._roomObject = roomObject
         // For now, the rest of the properties are hardcoded
-        self.title = "Scan 1"
-        self.type = "Room"
-        // TODO: missing id property
+        self._title = "Scan 1"
+        self._type = "Room"
     }
     
     public var title: String {
@@ -92,7 +48,7 @@ public class Room {
         }
     }
     
-    public var roomObject: String {
+    public var roomObject: MDLAsset {
         get {
             return self._roomObject;
         }
