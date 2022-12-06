e01ebbde88711d889e9a88631d3c60e74f95709a
Juan Trebolle <jtr@jysk.com>
2022-12-06 20:26:27 -0300

    adding mock text to see how it looks

--- LiSIGN/View/Pages/RoomDetailed.swift
+++ LiSIGN/View/Pages/RoomDetailed.swift
@@ -11,8 +11,13 @@ struct RoomDetailed: View {
     
     var body: some View {
         VStack{
-            Text("Room Detailed page")
             SceneView(scene: SCNScene(named: "fender_stratocaster.usdz"), options: [.autoenablesDefaultLighting, .allowsCameraControl])
+            
+            Spacer()
+            
+            Text("Room name")
+            Text("Description")
+            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
         }
         .frame(width: 400, height: 400)
     }
